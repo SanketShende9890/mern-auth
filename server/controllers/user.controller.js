03254c6dec18d0c79a0f0df6159981703e44c97d
sanket <Sanket137>
2024-08-05 17:24:45 +0530

    created test route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+    res.send("<h3>Server is running fine!!!</h3>");
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 dotenv.config();    
 
 
@@ -13,9 +14,8 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to database");
 }).catch(err=>console.log(err))
 
-app.get('/', (req, res)=>{
-    res.send('Server is running!!');
-})
+app.use("/", userRouter)
+
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running on port: ${PORT}`);
 })
