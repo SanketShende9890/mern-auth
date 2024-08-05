833d0d2efd57834ee1025d29d895ab9962b05521
sanket <Sanket137>
2024-08-05 16:28:32 +0530

    added rotes and page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
     <div>
-      <h1 className="underline text-red-500 text-center">
-      App
-
-      </h1>
+     <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+     </BrowserRouter>
       </div>
   )
 }
