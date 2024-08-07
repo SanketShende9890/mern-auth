9fa562be45159bc6d83bdbad7dd446c22e901434
sanket <Sanket137>
2024-08-07 20:20:29 +0530

    created signup api routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 dotenv.config();    
@@ -11,7 +11,7 @@ const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect("mongodb+srv://sanketshende18:admin@cluster0.rw80cbl.mongodb.net/mern-auth-base").then(()=>{
     console.log("Connected to database");
 }).catch(err=>console.log(err))
 
